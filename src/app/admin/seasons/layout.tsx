6b68aabb1a9400057393df8af08d5b9af53b52dd
Ariel Aramayo <ariel.rr32@gmail.com>
2026-06-07 01:27:00 -0400

    refactor

--- src/app/admin/seasons/layout.tsx
+++ src/app/admin/seasons/layout.tsx
@@ -29,7 +29,7 @@ export default async function AdminLayout({
           </main>
         </div>
         {/* <!-- Mobile BottomNavBar --> */}
-        <BottonNavBar />
+        <BottonNavBar items={[]} urlBase={`/admin/seasons`} />
       </div>
     </>
   );
